3f57733cd0a70ec60a594b15f0b3981f203f52cf
weiShaoY_ <1604705673@qq.com>
2024-11-20 02:00:09 +0800

    chore: 🔧删除无用组件

--- src/components/icon/icon-button.tsx
+++ src/components/icon/icon-button.tsx
@@ -17,6 +17,7 @@ export default function IconButton({
 			style={style}
 			className={`flex cursor-pointer items-center justify-center rounded-full p-2 hover:bg-hover ${className}`}
 			onClick={onClick}
+			type="button"
 		>
 			{children}
 		</button>
--- src/components/progress-bar/index.tsx
+++ src/components/progress-bar/index.tsx
@@ -10,10 +10,26 @@ NProgress.configure({
 	showSpinner: false,
 });
 
+/**
+ * ProgressBar 组件
+ *
+ * 用于在路由变化时显示进度条效果。
+ */
 export default function ProgressBar() {
+	/**
+	 * 当前路由路径名
+	 */
 	const pathname = usePathname();
+
+	/**
+	 * 获取主题的主色
+	 */
 	const { colorPrimary } = useThemeToken();
 
+	/**
+	 * @description 更新 NProgress 样式，使其符合主题的主色调
+	 * @example  在路由变化时调用 updateProgressBarStyle();
+	 */
 	const updateProgressBarStyle = useCallback(() => {
 		const nprogress = document.getElementById("nprogress");
 		if (!nprogress) return;
@@ -28,20 +44,26 @@ export default function ProgressBar() {
 		peg.style.boxShadow = `0 0 10px ${colorPrimary}, 0 0 5px ${colorPrimary}`;
 	}, [colorPrimary]);
 
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
+		// 开始显示进度条
 		NProgress.start();
+		// 更新样式
 		updateProgressBarStyle();
 
-		// 路由变化完成时
+		/**
+		 * @description 模拟路由变化完成的异步效果
+		 */
 		const timer = setTimeout(() => {
-			NProgress.done();
+			NProgress.done(); // 完成进度条
 		}, 100);
 
 		return () => {
+			// 清除定时器并完成进度条
 			clearTimeout(timer);
 			NProgress.done();
 		};
-	}, [pathname, updateProgressBarStyle]);
+	}, [pathname, updateProgressBarStyle]); // 依赖路由变化和样式更新
 
-	return null;
+	return null; // 无需渲染任何内容
 }
