9d7a3dd810a7edb0062e26f7d5f1ed1f4db399f9
weiShaoY_ <1604705673@qq.com>
2024-11-24 02:38:57 +0800

    chore: 🔧首页头部调整

--- src/layouts/home/header/index.tsx
+++ src/layouts/home/header/index.tsx
@@ -1,12 +1,24 @@
-import Menu from "./components/menu";
-
-import { isMobile } from "@/utils";
+import MobileMenu from "./components/mobile-menu";
+import DesktopMenu from "./components/desktop-menu";
 
 import Logo from "@/components/logo";
 
 import Github from "@/components/github";
 
-import { NavLink } from "react-router-dom";
+const menuList = [
+	{
+		key: "/home/about",
+		label: "About",
+	},
+	{
+		key: "/home/resume",
+		label: "Resume",
+	},
+	{
+		key: "/code",
+		label: "Code",
+	},
+];
 
 export default function HomeHeader({ headerHeight = 80 }) {
 	return (
@@ -17,21 +29,12 @@ export default function HomeHeader({ headerHeight = 80 }) {
 			<div className="container flex items-center justify-between mx-5">
 				<Logo />
 
-				<div className="flex items-center gap-5 ">
-					<NavLink className="!text-white font-bold" to="/home/about">
-						About
-					</NavLink>
-
-					<NavLink className="!text-white font-bold" to="/home/resume">
-						Resume
-					</NavLink>
-				</div>
+				<DesktopMenu menuList={menuList} />
 
 				<div className="flex items-center gap-5">
 					<Github iconColor="#ffffff" />
-
 					{/* 下拉菜单,只有h5可见 */}
-					{isMobile && <Menu headerHeight={headerHeight} />}
+					<MobileMenu menuList={menuList} headerHeight={headerHeight} />
 				</div>
 			</div>
 		</header>
--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -9,7 +9,7 @@ export default function HomeLayout({ children }: Props) {
 	const headerHeight = 80;
 
 	return (
-		<div className="bg-[#212224]" style={{ paddingTop: `${headerHeight}px` }}>
+		<div className="bg-[#212224] h-full" style={{ paddingTop: `${headerHeight}px` }}>
 			<Header headerHeight={headerHeight} />
 			{children}
 		</div>
