e90330426fd2dce41d2447eb4a7ebadf54b6e4b9
weiShaoY_ <1604705673@qq.com>
2024-11-26 23:23:17 +0800

    chore: 🔧车库路由调整

--- src/router/routes/garage-route.tsx
+++ src/router/routes/garage-route.tsx
@@ -4,8 +4,6 @@ import { Outlet } from "react-router-dom";
 
 import { CircleLoading } from "@/components/loading";
 
-import SimpleLayout from "@/layouts/simple";
-
 import type { AppRouteObject } from "#/router";
 
 // 使用 React.lazy 动态加载错误页面组件
@@ -17,16 +15,12 @@ const GaragePage = lazy(() => import("@/pages/garage"));
  */
 export const GarageRoute: AppRouteObject = {
 	element: (
-		<SimpleLayout>
+		<Suspense fallback={<CircleLoading />}>
 			{" "}
-			{/* 使用简单布局包裹错误页面 */}
-			<Suspense fallback={<CircleLoading />}>
-				{" "}
-				{/* 懒加载时显示加载动画 */}
-				{/* 占位符，用于渲染子路由的组件 */}
-				<Outlet />
-			</Suspense>
-		</SimpleLayout>
+			{/* 懒加载时显示加载动画 */}
+			{/* 占位符，用于渲染子路由的组件 */}
+			<Outlet />
+		</Suspense>
 	),
 	children: [
 		{ path: "garage", element: <GaragePage /> }, // 403 页面路径和对应组件
