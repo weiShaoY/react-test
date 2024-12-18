419ea69ed6958014efabd84ae3655a1efc36b5b9
weiShaoY_ <1604705673@qq.com>
2024-12-18 15:22:13 +0800

    chore: 🔧投诉举报整理

--- src/pages/blog/socialEngineering/complain/index.tsx
+++ src/pages/blog/socialEngineering/complain/index.tsx
@@ -5,7 +5,7 @@ import Card from "@/components/card";
 const data = [
 	{
 		key: "1",
-		category: "公众服务电话号码、常用电话、紧急电话",
+		category: "紧急服务电话",
 		numbers: [
 			{ name: "火警电话", number: "119", website: "http://www.119.gov.cn/" },
 			{ name: "报警服务台", number: "110", website: "http://www.mps.gov.cn/" },
@@ -50,7 +50,7 @@ const data = [
 	},
 	{
 		key: "2",
-		category: "全国国家机构监督、投诉、抢修、举报电话",
+		category: "政府监管与投诉电话",
 		numbers: [
 			{
 				name: "电力系统客服电话",
@@ -101,7 +101,7 @@ const data = [
 	},
 	{
 		key: "3",
-		category: "全国银行客户服务电话号码",
+		category: "银行客户服务电话",
 		numbers: [
 			{
 				name: "招商银行",
@@ -154,10 +154,10 @@ const data = [
 	},
 	{
 		key: "5",
-		category: "全国通信机构服务电话号码",
+		category: "通信服务电话号码",
 		numbers: [
 			{
-				name: "中国电信客户服务热线",
+				name: "中国电信客服热线",
 				number: "10000",
 				website: "http://www.chinatelecom.com.cn/",
 			},
@@ -172,33 +172,18 @@ const data = [
 				website: "http://www.chinamobileltd.com/",
 			},
 			{
-				name: "中国电信IP电话卡",
-				number: "17900",
-				website: "http://www.chinatelecom.com.cn/",
-			},
-			{
-				name: "中国联通IP号码",
-				number: "17911",
-				website: "http://www.chinaunicom.com/",
-			},
-			{
-				name: "中国移动IP号码",
-				number: "17951",
+				name: "中国移动投诉电话",
+				number: "4400111086",
 				website: "http://www.chinamobileltd.com/",
 			},
 			{
-				name: "中国联通的“电话导航”业务",
-				number: "116114",
+				name: "中国联通投诉电话",
+				number: "440017910",
 				website: "http://www.chinaunicom.com/",
 			},
 			{
-				name: "中国电信号码百事通",
-				number: "118114",
-				website: "http://www.chinatelecom.com.cn/",
-			},
-			{
-				name: "铁通客户服务",
-				number: "10050",
+				name: "中国电信投诉电话",
+				number: "440017900",
 				website: "http://www.chinatelecom.com.cn/",
 			},
 			{
@@ -206,42 +191,33 @@ const data = [
 				number: "10086999",
 				website: "http://www.chinatelecom.com.cn/",
 			},
+			{
+				name: "中国电信服务质量监督热线",
+				number: "10080",
+				website: "http://www.chinatelecom.com.cn/",
+			},
+			{
+				name: "中国电信网络诈骗举报电话",
+				number: "12369",
+				website: "http://www.chinatelecom.com.cn/",
+			},
+			{
+				name: "中华人民共和国工业和信息化部服务电话",
+				number: "12381",
+				website: "https://www.chinatelecom.com.cn/",
+			},
 		],
 	},
 	{
 		key: "6",
-		category: "紧急救援电话",
-		numbers: [
-			{ name: "急救电话", number: "120", website: "http://www.120.gov.cn/" },
-			{ name: "火警电话", number: "119", website: "http://www.119.gov.cn/" },
-			{
-				name: "水上救援电话",
-				number: "12395",
-				website: "http://www.moc.gov.cn/",
-			},
-			{
-				name: "交通事故报警电话",
-				number: "122",
-				website: "http://www.mps.gov.cn/",
-			},
-			{
-				name: "紧急呼叫中心",
-				number: "112",
-				website: "http://www.china-travel.com.cn/",
-			},
-		],
-	},
-
-	{
-		key: "7",
 		category: "保险公司客服电话",
 		numbers: [
+			{ name: "平安保险", number: "95511", website: "https://www.pingan.com/" },
 			{
 				name: "中国人寿",
 				number: "95519",
 				website: "https://www.chinalife.com.cn/",
 			},
-			{ name: "平安保险", number: "95511", website: "https://www.pingan.com/" },
 			{
 				name: "太平洋保险",
 				number: "95500",
@@ -253,41 +229,17 @@ const data = [
 				website: "https://www.newchinalife.com/",
 			},
 			{ name: "人保财险", number: "95518", website: "https://www.picc.com/" },
+			{ name: "国寿财险", number: "12360", website: "https://www.gsxc.com/" },
+			{
+				name: "华泰保险",
+				number: "12368",
+				website: "https://www.hualicai.com/",
+			},
+			{ name: "国寿康泰", number: "12368", website: "https://www.gsxt.cn/" },
 		],
 	},
 	{
-		key: "8",
-		category: "健康服务热线",
-		numbers: [
-			{
-				name: "国家健康热线",
-				number: "12320",
-				website: "http://www.12320.gov.cn/",
-			},
-			{
-				name: "心理健康热线",
-				number: "12355",
-				website: "http://www.12355.com.cn/",
-			},
-			{
-				name: "老年人健康咨询",
-				number: "12349",
-				website: "http://www.12349.cn/",
-			},
-			{
-				name: "中医健康咨询",
-				number: "12320",
-				website: "http://www.12320.gov.cn/",
-			},
-			{
-				name: "妇幼保健热线",
-				number: "12344",
-				website: "http://www.12344.cn/",
-			},
-		],
-	},
-	{
-		key: "9",
+		key: "7",
 		category: "儿童保护热线",
 		numbers: [
 			{
@@ -308,8 +260,8 @@ const data = [
 		],
 	},
 	{
-		key: "10",
-		category: "环境保护电话",
+		key: "8",
+		category: "环境保护热线",
 		numbers: [
 			{
 				name: "环保举报电话",
@@ -339,247 +291,216 @@ const data = [
 		],
 	},
 	{
-		key: "11",
-		category: "交通服务电话号码",
+		key: "9",
+		category: "交通服务电话",
 		numbers: [
 			{
 				name: "高速公路服务热线",
 				number: "12122",
-				website: "http://www.china-highway.com/",
+				website: "http://www .12122.gov.cn/",
 			},
 			{
-				name: "地铁咨询电话",
-				number: "12345",
-				website: "https://www.metrochina.com/",
+				name: "出租车投诉电话",
+				number: "12328",
+				website: "http://www.mot.gov.cn/",
 			},
 			{
-				name: "铁路售票电话",
-				number: "12306",
-				website: "https://www.12306.cn/",
-			},
-			{
-				name: "公共交通服务热线",
-				number: "96220",
-				website: "http://www.buscn.com/",
-			},
-			{
-				name: "飞机航班服务电话",
-				number: "95583",
-				website: "https://www.china-airlines.com/",
-			},
-		],
-	},
-	{
-		key: "12",
-		category: "法律服务电话号码",
-		numbers: [
-			{
-				name: "法律援助热线",
-				number: "12348",
-				website: "http://www.12348.gov.cn/",
-			},
-			{
-				name: "法律咨询服务",
-				number: "400-665-1818",
-				website: "http://www.lawyer.com.cn/",
-			},
-			{
-				name: "律师服务电话",
-				number: "400-060-9056",
-				website: "http://www.lawyer.com.cn/",
-			},
-			{
-				name: "公安举报热线",
-				number: "12389",
-				website: "https://www.police.gov.cn/",
-			},
-			{
-				name: "刑事案件举报电话",
-				number: "12358",
-				website: "http://www.ciic.cn/",
-			},
-		],
-	},
-	{
-		key: "13",
-		category: "全国交通管理服务热线",
-		numbers: [
-			{
-				name: "全国交通违法举报",
+				name: "公共交通服务",
 				number: "12319",
-				website: "http://www.12319.cn/",
+				website: "http://www.cntrc.org.cn/",
 			},
 			{
-				name: "交通事故处理咨询",
+				name: "航空公司客户服务热线",
+				number: "95338",
+				website: "http://www.airchina.com.cn/",
+			},
+			{
+				name: "铁路客户服务",
+				number: "12306",
+				website: "http://www.12306.cn/",
+			},
+			{
+				name: "交通运输部交通安全热线",
+				number: "12328",
+				website: "http://www.mot.gov.cn/",
+			},
+			{
+				name: "交通管理部门电话",
 				number: "122",
 				website: "http://www.mps.gov.cn/",
-			},
-			{
-				name: "交通事故报警电话",
-				number: "122",
-				website: "http://www.mps.gov.cn/",
-			},
-			{
-				name: "高速公路服务热线",
-				number: "12122",
-				website: "http://www.china-highway.com/",
 			},
 		],
 	},
 	{
-		key: "15",
-		category: "天气预报与灾害预警电话",
+		key: "10",
+		category: "健康医疗热线",
 		numbers: [
 			{
-				name: "全国气象服务电话",
-				number: "12121",
-				website: "http://www.nmc.cn/",
+				name: "国家卫生健康委员会",
+				number: "12320",
+				website: "http://www.nhc.gov.cn/",
 			},
-			{
-				name: "台风预警电话",
-				number: "12360",
-				website: "http://www.typhoon.org.cn/",
-			},
-			{
-				name: "天气预警电话",
-				number: "12121",
-				website: "http://www.nmc.cn/",
-			},
-			{
-				name: "自然灾害报告电话",
-				number: "12322",
-				website: "http://www.scd.gov.cn/",
-			},
-		],
-	},
-	{
-		key: "14",
-		category: "全国医疗服务电话",
-		numbers: [
 			{
 				name: "疾病预防控制中心",
 				number: "12320",
 				website: "http://www.chinacdc.cn/",
 			},
 			{
-				name: "急诊救护中心",
-				number: "120",
-				website: "http://www.120.gov.cn/",
+				name: "全国心理援助热线",
+				number: "12355",
+				website: "http://www.12355.com.cn/",
 			},
 			{
 				name: "中国红十字会",
-				number: "400-668-6000",
+				number: "12333",
 				website: "http://www.redcross.org.cn/",
 			},
 			{
-				name: "医疗保险咨询",
-				number: "12333",
-				website: "http://www.nhsa.gov.cn/",
+				name: "全国计生热线",
+				number: "12356",
+				website: "http://www.npf.org.cn/",
+			},
+			{
+				name: "老年人健康热线",
+				number: "12349",
+				website: "http://www.12349.com.cn/",
+			},
+			{
+				name: "公共卫生服务中心",
+				number: "12320",
+				website: "http://www.chinacdc.cn/",
+			},
+		],
+	},
+	{
+		key: "11",
+		category: "金融监管与举报热线",
+		numbers: [
+			{
+				name: "中国银监会",
+				number: "12378",
+				website: "http://www.cbrc.gov.cn/",
+			},
+			{
+				name: "中国证监会",
+				number: "12386",
+				website: "http://www.csrc.gov.cn/",
+			},
+			{
+				name: "中国人民银行",
+				number: "95566",
+				website: "http://www.pbc.gov.cn/",
+			},
+			{
+				name: "金融消费者权益保护热线",
+				number: "12345",
+				website: "http://www.gov.cn/",
+			},
+		],
+	},
+	{
+		key: "12",
+		category: "食品安全与举报热线",
+		numbers: [
+			{
+				name: "食品安全举报热线",
+				number: "12331",
+				website: "http://www.sfda.gov.cn/",
+			},
+			{
+				name: "国家市场监管总局",
+				number: "12315",
+				website: "http://www.samr.gov.cn/",
+			},
+			{
+				name: "质量检验举报热线",
+				number: "12365",
+				website: "http://www.cnca.gov.cn/",
+			},
+		],
+	},
+	{
+		key: "13",
+		category: "消费者权益保护",
+		numbers: [
+			{
+				name: "消费者投诉电话",
+				number: "12315",
+				website: "http://www.12315.cn/",
+			},
+			{
+				name: "全国知识产权保护电话",
+				number: "12330",
+				website: "http://www.sipo.gov.cn/",
+			},
+			{
+				name: "全国广告投诉电话",
+				number: "12316",
+				website: "http://www.samr.gov.cn/",
+			},
+			{
+				name: "中国消费者协会",
+				number: "010-59799616",
+				website: "http://www.cca.org.cn/",
+			},
+		],
+	},
+	{
+		key: "14",
+		category: "紧急救援与灾难响应",
+		numbers: [
+			{
+				name: "地震应急救援",
+				number: "12122",
+				website: "http://www.cenc.ac.cn/",
+			},
+			{
+				name: "灾害紧急响应",
+				number: "12398",
+				website: "http://www.ndrc.gov.cn/",
+			},
+			{
+				name: "洪水灾害紧急热线",
+				number: "12304",
+				website: "http://www.mwr.gov.cn/",
 			},
 		],
 	},
 	{
 		key: "15",
-		category: "全国消防服务电话",
+		category: "网络安全与举报热线",
 		numbers: [
 			{
-				name: "火警电话",
-				number: "119",
-				website: "http://www.119.gov.cn/",
+				name: "全国网络举报热线",
+				number: "12377",
+				website: "http://www.cyberpolice.cn/",
 			},
+			{ name: "反诈骗热线", number: "12321", website: "http://www.12321.cn/" },
 			{
-				name: "消防安全举报电话",
-				number: "12345",
-				website: "http://www.firechina.gov.cn/",
-			},
-			{
-				name: "消防技术服务电话",
-				number: "010-64366218",
-				website: "http://www.mps.gov.cn/",
+				name: "国家互联网信息办公室",
+				number: "12377",
+				website: "http://www.cac.gov.cn/",
 			},
 		],
 	},
 	{
 		key: "16",
-		category: "全国居民生活服务电话",
+		category: "环境保护与公益举报",
 		numbers: [
 			{
-				name: "生活垃圾分类咨询电话",
-				number: "12319",
-				website: "http://www.12319.gov.cn/",
+				name: "环保举报电话",
+				number: "12369",
+				website: "http://www.mee.gov.cn/",
 			},
 			{
-				name: "水务热线",
-				number: "12319",
-				website: "http://www.waterchina.com/",
+				name: "生态环境保护热线",
+				number: "12300",
+				website: "http://www.mee.gov.cn/",
 			},
 			{
-				name: "全国电力服务热线",
-				number: "95598",
-				website: "http://www.sgcc.com.cn/",
-			},
-			{
-				name: "燃气安全热线",
-				number: "12319",
-				website: "http://www.china-gas.com/",
-			},
-		],
-	},
-	{
-		key: "17",
-		category: "全国消费者权益保护电话",
-		numbers: [
-			{
-				name: "消费者投诉热线",
-				number: "12315",
-				website: "http://www.12315.cn/",
-			},
-			{
-				name: "质量投诉电话",
-				number: "12365",
-				website: "http://www.cnca.gov.cn/",
-			},
-			{
-				name: "食品安全投诉电话",
-				number: "12331",
-				website: "http://www.cfda.gov.cn/",
-			},
-		],
-	},
-	{
-		key: "18",
-		category: "全国邮政服务电话",
-		numbers: [
-			{
-				name: "邮政快递服务电话",
-				number: "11183",
-				website: "http://www.ems.com.cn/",
-			},
-			{
-				name: "邮政服务咨询电话",
-				number: "11185",
-				website: "http://www.china-post.com.cn/",
-			},
-			{
-				name: "邮政储蓄银行电话",
-				number: "95580",
-				website: "http://www.psbc.com/",
-			},
-		],
-	},
-	{
-		key: "19",
-		category: "全国公路与交通安全热线",
-		numbers: [
-			{
-				name: "公路交通安全咨询电话",
-				number: "12328",
-				website: "http://www.chinahighway.com/",
-			},
-			{
-				name: "汽车保险热线",
-				number: "95518",
-				website: "http://www.picc.com.cn/",
+				name: "公益服务投诉热线",
+				number: "12370",
+				website: "http://www.ngo.cn/",
 			},
 		],
 	},
